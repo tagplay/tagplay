5bcdbdb6f96b769c6a0e8e91c394deacbf1b55f0
Fannar Snær <fannar@lambagras.is>
2019-03-15 14:05:38 +0000

    1.2.2
    Updating SuperAgent dependency
     - Prevents zip bombs

--- request.js
+++ request.js
@@ -25,7 +25,7 @@ function submit(method, url, token, options, cb) {
   if (token) {
     req.set('Authorization', 'Bearer ' + token);
   }
-  req.timeout(30 * 1000); // 30 seconds
+  req.timeout({ response: 30 * 1000 }); // 30 seconds
   req.end(function(err, response) {
     if (err) {
       return cb(err);
